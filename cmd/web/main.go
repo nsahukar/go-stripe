5e060ba1efe9cab4f54ca712dbaf3d040dc0b70d
Nikhil Sahukar <nsahukar@gmail.com>
2021-09-02 19:58:17 +0530

    Basic project and web server setup

---
+++ cmd/web/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env string 		// development or production
+	api string 		// backend url
+	db struct {
+		dsn string 	// database connection string
+	}
+	stripe struct {
+		secret string
+		key string
+	}
+}
+
+type application struct {
+	config config
+	infoLog *log.Logger
+	errorLog *log.Logger
+	templateCache map[string]*template.Template
+	version string
+}
+
+func (app *application) serve() error {
+	srv := http.Server {
+		Addr: fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+		IdleTimeout: 30 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	app.infoLog.Printf("Starting HTTP server in %s mode on port %d\n", app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
+
+
+func main() {
+	// Setting up configuration options
+	var cfg config
+
+	// Get port, environment, and api URL from command flags
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+	flag.Parse()
+
+	// Getting stripe info from environment vars
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	// Setting up info and error logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Setting up template cache
+	tc := make(map[string]*template.Template)
+
+	// Setting up application
+	app := &application{
+		config: cfg,
+		infoLog: infoLog,
+		errorLog: errorLog,
+		templateCache: tc,
+		version: version,
+	}
+
+	err := app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nsahukar/go-stripe
+
+go 1.15
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.4
+)
